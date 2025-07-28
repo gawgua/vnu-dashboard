6818cd003e11fbcda99ee6d5489e82ac09eee0f4
gawgua <taisaodeoduoc@gmail.com>
2025-07-28 05:49:58 +0000

    feat: home page

---
+++ app/components/GPAChart.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
+import { BedIcon } from "lucide-react";
+import { 
+	LineChart, 
+	Line, 
+	XAxis, 
+	CartesianGrid, 
+	LabelList,
+	YAxis,
+	Legend
+} from "recharts";
+
+export default function GPAChart({data}: {
+	data: { id: string; tenHocKy: string; tongket: number; tichluy: number }[];
+}) {
+	const chartConfig: ChartConfig = {
+		tongket: {
+			label: "GPA Học kỳ",
+			color: "#2563eb",
+		},
+		tichluy: {
+			label: "GPA Tích lũy", 
+			color: "#dc2626",
+		},
+	};
+
+	return (
+		<Card>
+			<CardContent>
+				<ChartContainer config={chartConfig}>
+					<LineChart accessibilityLayer data={data} margin={{ top: 5, right: 80, left: 25 }}>
+						<CartesianGrid vertical={false} stroke="black" strokeDasharray="4 5" strokeWidth={0.5}/>
+						<XAxis 
+							dataKey="tenHocKy" 
+							tickLine={false} 
+							interval={0}
+							/>
+						<YAxis 
+							domain={[0, 4]} 
+							tickCount={6} 
+							tickLine={false} 
+							axisLine={false} 
+							/>
+						<Legend />
+						<Line type="linear" dataKey="tongket" stroke={chartConfig.tongket.color} strokeWidth={2}>
+							<LabelList position="bottom" />
+						</Line>
+						<Line type="linear" dataKey="tichluy" stroke={chartConfig.tichluy.color} strokeWidth={2}>
+							<LabelList position="top" />
+						</Line>
+					</LineChart>
+				</ChartContainer>
+			</CardContent>
+		</Card>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import { APIHandler } from "@/lib/APIHandler";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { donVi, heDaoTao, nganhDaoTao } from "@/lib/constants";
+import GPAChart from "./components/GPAChart";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 
 export default async function HomePage() {
 	const token = (await cookies()).get("accessToken")?.value;
@@ -11,14 +15,53 @@ export default async function HomePage() {
 
 	const apiHandler = new APIHandler(token, refreshToken);
 
-	const { soKyDaHoc, diemTrungBinhHe4TichLuy, tongSoTinChiTichLuy } = (await apiHandler.getTongKetDenHienTai())[0];
+	const tongket = (await apiHandler.getTongKetDenHienTai())[0];
+	const svInfo = await apiHandler.getInfoSinhVien();
+	const classData = (await apiHandler.getDataLopDaoTao(
+		svInfo.idLopDaoTao,
+		svInfo.guidDonVi,
+		svInfo.idBacDaoTao,
+		svInfo.idHeDaoTao,
+		svInfo.idNganhDaoTao,
+		svInfo.idNienKhoaDaoTao,
+		svInfo.idChuongTrinhDaoTao
+	))[0];
+
+	const danhSachHocKy = await apiHandler.getDanhSachHocKyTheoDiem();
+	const gpaTongKet = [];
+
+	for (const hocKy of danhSachHocKy) {
+		const tongket = (await apiHandler.getDiemTrungBinhHocKy(hocKy.id))[0];
+		gpaTongKet.push({
+			id: hocKy.id,
+			tenHocKy: `Học kỳ ${hocKy.ten} năm học ${hocKy.nam}`,
+			tongket: Number.parseFloat(tongket.diemTrungBinhHe4_HocKy),
+			tichluy: Number.parseFloat(tongket.diemTrungBinhHe4_TichLuyDenHocKyHienTai)
+		});
+	}
+	gpaTongKet.sort((a, b) => Number(a.id) - Number(b.id));
 
 	return (
-		<div>
-			<h1>Trang chủ</h1>
-			<p>Số kỳ đã học: {soKyDaHoc}</p>
-			<p>Điểm trung bình hệ 4 tích lũy: {diemTrungBinhHe4TichLuy}</p>
-			<p>Tổng số tín chỉ tích lũy: {tongSoTinChiTichLuy}</p>
+		<div className="w-full space-y-4 mr-2 mt-2.25 mb-2.25">
+			<Card className="gap-3">
+				<CardHeader className="text-xl font-bold">Thông tin người học</CardHeader>
+				<CardContent className="space-y-1">
+					<div><span className="font-bold">Họ tên:</span> {svInfo.hoVaTen}</div>
+					<div><span className="font-bold">Đơn vị:</span> {donVi.find((dv) => dv.guid === svInfo.guidDonVi)!.tenDonVi}</div>
+					<div><span className="font-bold">Ngành học:</span> {nganhDaoTao.find((ndt) => ndt.id === svInfo.idNganhDaoTao && ndt.guidDonVi === svInfo.guidDonVi)!.ten}</div>
+					<div><span className="font-bold">Lớp khóa học:</span> {classData.ten} ({classData.tenVietTat})</div>
+				</CardContent>
+			</Card>
+			<Card className="bg-primary text-white font-semibold p-1.5 border-0">
+				<CardContent className="flex items-center justify-center space-x-20">
+					<div>Số kỳ đã học: {tongket.soKyDaHoc}</div>
+					<Separator orientation="vertical" className="min-h-10" />
+					<div>Điểm trung bình tích lũy: {tongket.diemTrungBinhHe4TichLuy}</div>
+					<Separator orientation="vertical" className="min-h-10"/>
+					<div>Tín chỉ tích lũy: {tongket.tongSoTinChiTichLuy}</div>
+				</CardContent>
+			</Card>
+			<GPAChart data={gpaTongKet} />
 		</div>
 	);
 }
