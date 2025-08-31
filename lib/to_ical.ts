bc156570f707332127684757de491378df78d312
gawgua <taisaodeoduoc@gmail.com>
2025-08-31 15:36:46 +0000

    fix: calender export timezone error
    the day got shift up 1 day because the time is already UTC but i minus as it is UTC+7 so it became UTC-7 and Mon -> Tue, etc..., but the RRULES BYDAY property still at UTC+7 day

--- lib/to_ical.ts
+++ lib/to_ical.ts
@@ -5,9 +5,8 @@ import { PeriodTime } from "./constants";
 
 const days = ["MO", "TU", "WE", "TH", "FR", "SA", "SU"];
 const SEC_PER_DAY = 24 * 60 * 60;
-const GMT7_OFFSET = 7 * 60 * 60;
 
-export function toCalendar(data: ThoiKhoaBieuResponse[], startDate: Date, endDate: Date, periodTime: PeriodTime[]): string {
+export function toCalendar(data: ThoiKhoaBieuResponse[], startDate: Date, totalWeeks: number, periodTime: PeriodTime[]): string {
 	const events: EventAttributes[] = [];
 
 	for (const event of data) {
@@ -20,23 +19,27 @@ export function toCalendar(data: ThoiKhoaBieuResponse[], startDate: Date, endDat
 		if (delta < 0) {
 			delta += 7;
 		}
-
+		
+		const endDate = new Date(startDate.getTime() + (totalWeeks * 7 - 1) * SEC_PER_DAY * 1000);
 		let endDateStr = endDate.toISOString();
 		endDateStr = endDateStr.replaceAll("-", "").replaceAll(":", "").slice(0, endDateStr.length - 9) + "Z"; 
 		const e: EventAttributes = {
 			title: event.tenHocPhan + (event.nhom === "0"? "" : ` (Nhóm ${event.nhom})`),
 			location: event.tenPhong,
-			start: new Date(startDate.getTime() + delta * SEC_PER_DAY * 1000 - GMT7_OFFSET * 1000).setHours(
+			start: new Date(startDate.getTime() + delta * SEC_PER_DAY * 1000).setHours(
 				Number.parseInt(startTime.split(":")[0]),
 				Number.parseInt(startTime.split(":")[1])
 			),
-			end: new Date(startDate.getTime() + delta * SEC_PER_DAY * 1000 - GMT7_OFFSET * 1000).setHours(
+			startInputType: "local",
+			startOutputType: "utc",
+			end: new Date(startDate.getTime() + delta * SEC_PER_DAY * 1000).setHours(
 				Number.parseInt(endTime.split(":")[0]),
 				Number.parseInt(endTime.split(":")[1])
 			),
+			endInputType: "local",
+			endOutputType: "utc",
 			recurrenceRule: `FREQ=WEEKLY;BYDAY=${days[Number.parseInt(event.ngayTrongTuan) - 1]};UNTIL=${endDateStr}`,
 		}
-
 		events.push(e);
 	}
 
